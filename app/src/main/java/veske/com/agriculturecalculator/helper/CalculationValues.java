4a39d19e6393a737a3c131bd385d51e1c2ce5a6a
Kaspar Tint <kaspar.tint@gmail.com>
2015-09-19 18:14:55 +0300

    feature/D-0000:    * Base logic for HelperActivity

---
+++ app/src/main/java/veske/com/agriculturecalculator/helper/CalculationValues.java
@@ -0,0 +1,68 @@
+package veske.com.agriculturecalculator.helper;
+
+import android.content.res.Resources;
+
+import veske.com.agriculturecalculator.R;
+
+public class CalculationValues {
+
+    private float[] N;
+    private float[] P;
+    private float[] K;
+    private float[] Mg;
+    private float[] S;
+    private float[] X;
+
+    private Resources resources;
+
+    CalculationValues(Resources resources) {
+        this.resources = resources;
+
+        String[] n = resources.getStringArray(R.array.N);
+        String[] p = resources.getStringArray(R.array.P);
+        String[] k = resources.getStringArray(R.array.K);
+        String[] mg = resources.getStringArray(R.array.Mg);
+        String[] s = resources.getStringArray(R.array.S);
+        String[] x = resources.getStringArray(R.array.harvestValues);
+
+        N = new float[n.length];
+        P = new float[p.length];
+        K = new float[k.length];
+        Mg = new float[mg.length];
+        S = new float[s.length];
+        X = new float[x.length];
+
+        for (int i = 0; i < n.length; i++) {
+            N[i] = Float.parseFloat(n[i]);
+            P[i] = Float.parseFloat(p[i]);
+            K[i] = Float.parseFloat(k[i]);
+            Mg[i] = Float.parseFloat(mg[i]);
+            S[i] = Float.parseFloat(s[i]);
+            X[i] = Float.parseFloat(x[i].replace(" tn/ha", ""));
+        }
+    }
+
+    public float[] getN() {
+        return N;
+    }
+
+    public float[] getP() {
+        return P;
+    }
+
+    public float[] getK() {
+        return K;
+    }
+
+    public float[] getMg() {
+        return Mg;
+    }
+
+    public float[] getS() {
+        return S;
+    }
+
+    public float[] getX() {
+        return X;
+    }
+}
--- app/src/main/java/veske/com/agriculturecalculator/helper/HelperActivity.java
+++ app/src/main/java/veske/com/agriculturecalculator/helper/HelperActivity.java
@@ -1,19 +1,87 @@
 package veske.com.agriculturecalculator.helper;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
 
 import veske.com.agriculturecalculator.R;
 
 @OptionsMenu(R.menu.menu_helper)
 @EActivity(R.layout.activity_helper)
-public class HelperActivity extends AppCompatActivity {
+public class HelperActivity extends AppCompatActivity implements OnItemSelectedListener {
+
+    @ViewById(R.id.cultureSpinner)
+    Spinner cultureSpinner;
+    @ViewById(R.id.harvestSpinner)
+    Spinner harvestSpinner;
+    @ViewById(R.id.n_value)
+    TextView N;
+
+    CalculationValues calculationValues;
+
+    boolean cultureSelected, harvestSelected = false;
+    int culturePos, harvestPos;
 
     @AfterViews
     public void initialize() {
-        // fds
+        initializeSpinners();
+        calculationValues = new CalculationValues(getResources());
+    }
+
+    public void initializeSpinners() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cultures, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cultureSpinner.setAdapter(adapter);
+        cultureSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.harvestValues, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        harvestSpinner.setAdapter(adapter2);
+        harvestSpinner.setOnItemSelectedListener(this);
+
+    }
+
+    private void calculate() {
+        float x = calculationValues.getX()[harvestPos];
+        N.setText(String.valueOf(calculationValues.getN()[culturePos] * x));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
+        switch (adapter.getId()) {
+            case R.id.cultureSpinner: {
+                culturePos = position;
+                if (cultureSelected && harvestSelected) {
+                    calculate();
+                } else {
+                    cultureSelected = true;
+                }
+
+                break;
+            }
+            case R.id.harvestSpinner: {
+                harvestPos = position;
+                if (cultureSelected && harvestSelected) {
+                    calculate();
+                } else {
+                    harvestSelected = true;
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapter) {
+
     }
 }
